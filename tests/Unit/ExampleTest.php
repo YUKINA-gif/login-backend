996fa9d5982bf9233a3be334ee175573d68f7e75
Yukina Nakanishi <yukina-nakanishi@outlook.jp>
2021-05-23 17:19:12 +0900

    テスト追加

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -2,17 +2,61 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
+use Tests\TestCase;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\DB;
 
 class ExampleTest extends TestCase
 {
     /**
-     * A basic test example.
+     * データベース準備
      *
      * @return void
+     *
      */
-    public function testBasicTest()
+    public function setUp(): void
     {
-        $this->assertTrue(true);
+        parent::setUp();
+        $user = [
+            "name" => "test",
+            "email" => "test@test.com",
+            "password" => Hash::make("testtest"),
+            "profile" => "Hello World"
+        ];
+
+        DB::table("users")->insert($user);
     }
+
+    /**
+     * ログインテスト
+     * 
+     * 正常系
+     *
+     * @return void
+     * @test
+     */
+    public function 正常系_ステータスコード200_login_post()
+    {
+        $user_data = [
+            "email" => "test@test.com",
+            "password" => "testtest",
+        ];
+        $response = $this->post("api/login", $user_data);
+        $response->assertStatus(200)->assertJsonFragment([
+            "auth" => true
+        ]);
+    }
+
+    /**
+     * データリフレッシュ
+     * 
+     * @return void
+     */
+    public function tearDown(): void
+    {
+        Artisan::call('migrate:refresh');
+        parent::tearDown();
+    }
+
 }
